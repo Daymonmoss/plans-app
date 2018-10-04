124065973f1f67ca7cf8d04513587e9acb41eb36
alpatovdanila <foh5gouz>
2018-10-04 11:45:54 +0300

    deploy

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 80;
 const hostname = process.env.HEROKU_APP_NAME || 'localhost'
-const protocol = !!process.env.HEROKU_APP_NAME ? 'https' : 'http';
+const protocol = !!process.env.HEROKU_APP_NAME ? 'http' : 'http';
 const AppConfig = {
 	spoolStore: true,
 	localStorageKey: 'APP_TEST',
